5b22489b4b4f70358b869245b87743783047d027
kimtaeho <>
2021-05-20 16:20:24 +0900

    gggg

--- app/src/main/java/com/sugoigroup/del1/App.kt
+++ app/src/main/java/com/sugoigroup/del1/App.kt
@@ -10,7 +10,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
-import org.koin.dsl.module
+import org.koin.dsl.moduleww
 
 class App : Application() {
     override fun onCreate() {
